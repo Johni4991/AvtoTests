cc098a9ed833b7aa91214f785ee3a29888c5f42b
evgenijgorbunov <gorbunov4991@mail.ru>
2024-01-15 12:22:52 +0400

    final commit

--- src/test/java/github/Lesson4.java
+++ src/test/java/github/Lesson4.java
@@ -1,18 +1,24 @@
 package github;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Lesson4 {
     @Test
     void BuildLikeTheBest(){
         Configuration.holdBrowserOpen = true;
         open("https://github.com/");
-        $("").hover();
-        $("href='/enterprise'").click();
-        $("#hero-section-brand-heading").shouldHave(Condition.text("The AI-powered"));
+        $$(".d-lg-flex").get(1).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
+        String expectedUrl = "https://github.com/enterprise";
+        String currentUrl = url();
+        assertEquals(expectedUrl, currentUrl);
+
     }
 }
